42ffd1e7620adb64696f95983e4405a77ed25d6b
SirEdvin <me@siredvin.site>
2025-02-11 13:08:29 +0200

    feat: add more groups to my repo

--- src/main/kotlin/site.siredvin.base.gradle.kts
+++ src/main/kotlin/site.siredvin.base.gradle.kts
@@ -8,6 +8,7 @@ repositories {
         name = "SirEdvin's Minecraft repository"
         content {
             includeGroup("cc.tweaked")
+            includeGroup("dan200.computercraf")
             includeGroup("org.squiddev")
             includeGroup("fuzs.forgeconfigapiport")
             includeGroup("site.siredvin")
